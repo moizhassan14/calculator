a08b431ca2f04804bc6684b4eb4c0d51870955df
moiz hassan <118419901+moizhassan14@users.noreply.github.com>
2023-05-24 00:56:48 +0500

    Add files via upload

---
+++ calculator/app.js
@@ -0,0 +1,28 @@
+var display = document.getElementById("display");
+var expression = "";
+
+function appendNumber(number) {
+  expression += number;
+  display.value = expression;
+}
+
+function appendOperator(operator) {
+  expression += operator;
+  display.value = expression;
+}
+
+function clearDisplay() {
+  expression = "";
+  display.value = "";
+}
+
+function calculate() {
+  try {
+    var result = eval(expression);
+    display.value = result;
+    expression = result.toString();
+  } catch (error) {
+    display.value = "Error";
+    expression = "";
+  }
+}
